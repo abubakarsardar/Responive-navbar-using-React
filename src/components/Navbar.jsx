891edd400ebf9ce34453f8aab18a93017527a644
Muhammad Abubakar <abubakarsardar04@gmail.com>
2025-04-11 19:15:16 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+
+
+export const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleButtonToggle = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <header>
+      <div className="container">
+        <div className="grid navbar-grid">
+          <div className="logo">
+            <h1>THAPA TECHNICAL</h1>
+          </div>
+
+          <nav className={showMenu ? "menu-mobile" : "menu-web"}>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#" > Contact</a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="ham-menu">
+            <button onClick={handleButtonToggle}>
+            <RxHamburgerMenu />
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
